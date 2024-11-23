8dfbfc31364ed3f51642bedd80c99b5f0d778ad2
AlineRG <aline.rabago@gmail.com>
2024-11-23 16:21:00 -0500

    Add GET route for '/pets' with authentication

--- routes/pets.js
+++ routes/pets.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-route.post('/pets')
-route.get('/pets')
-route.put('/pets')
-route.delete('/pets')
+// route.post('/pets')
+// route.get('/pets')
+// route.put('/pets')
+// route.delete('/pets')
+
+// GET /pets
+router.get("/pets", AuthenticationMiddleware, (req, res, next) => {
+    res.render("pets", { title: "Add a new pet" , user: req.user });
+  });
+
+module.exports = router;
