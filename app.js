5846d2f7384890074fe204db03c063a848a04f4a
AlineRG <aline.rabago@gmail.com>
2024-11-23 18:37:24 -0500

    Add import User model

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose'); //Connection to mongoose
+var User = require('./models/user'); 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
