aa3d91265e699fe6238b6e3272abecbc4ae3b5b8
AlineRG <aline.rabago@gmail.com>
2024-11-23 18:38:23 -0500

    Define user schema

--- models/user.js
+++ models/user.js
@@ -2,3 +2,9 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs'); // Encrypte password
 var Schema = mongoose.Schema;
 
+// Define user schema
+var userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
