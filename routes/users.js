de2f3ce27906cc8ef0858748ee38fda73e2250e4
AlineRG <aline.rabago@gmail.com>
2024-11-23 18:27:28 -0500

    Import User model and Passport for authentication

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');  // Import user model
+var passport = require('passport');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// Route to log in 
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/home',   //Succesful login
+  failureRedirect: '/users/login',  // Fail login
+}));
 
 module.exports = router;
