09619338db84f7cff2fde83f1b0ec96796828532
AlineRG <aline.rabago@gmail.com>
2024-11-23 18:22:22 -0500

    Configure Passport authentication with LocalStrategy

--- configs/passport.js
+++ configs/passport.js
@@ -5,3 +5,24 @@ var LocalStrategy = require('passport-local').Strategy;
 // Import user model
 var User = require('./models/user');
 
+// Config authentication
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.findOne({ username: username }, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false, { message: 'Incorrect username.' }); }
+        if (user.password !== password) { return done(null, false, { message: 'Incorrect password.' }); }
+        return done(null, user);
+      });
+    }
+  ));
+  
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
