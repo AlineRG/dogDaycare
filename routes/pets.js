8efebfc13603063331c2b0d3901199d996cd0437
AlineRG <aline.rabago@gmail.com>
2024-11-24 17:52:52 -0500

    Delete comments

--- routes/pets.js
+++ routes/pets.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-// route.post('/pets')
-// route.get('/pets')
-// route.put('/pets')
-// route.delete('/pets')
 
 // GET /pets
 router.get("/pets", AuthenticationMiddleware, (req, res, next) => {
