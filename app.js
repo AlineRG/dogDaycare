ce5439cfcc45de55b69502f8e13e022b92583972
AlineRG <aline.rabago@gmail.com>
2024-11-24 17:56:07 -0500

    Delete unnecessary pets and login handling

--- app.js
+++ app.js
@@ -141,16 +141,9 @@ passport.deserializeUser(async function(id, done) {
   }
 });
 
-// Routes in Home page
-
 // Route for Pets
-router.get('/pets', (req, res) => {
   if (req.isAuthenticated()) {
-    res.render('pets');
-  } else {
-    res.redirect('/login');
   }
-});
 
 // Route for reservations
 router.get('/reservations', (req, res) => {
