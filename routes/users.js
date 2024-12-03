349afa9b52e896f60769e157f2dc6a1f1d37e8ef
AlineRG <aline.rabago@gmail.com>
2024-12-02 21:19:09 -0500

    Added User model and bcrypt dependencies

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var User = require('../models/user');
+var bcrypt = require('bcryptjs');
 
 // Route to user model
 router.get('/register', function(req, res, next) {
