9470bad420c8b053bd7a1f1aa578a2a719ef5070
AlineRG <aline.rabago@gmail.com>
2024-12-15 22:15:03 -0500

    Update callbackURL

--- configs/globals.js
+++ configs/globals.js
@@ -1,16 +1,32 @@
+// require('dotenv').config();
+
+// const configurations = {
+//   ConnectionStrings: {
+//     MongoDB: process.env.MONGODB_URI || "mongodb+srv://alinerabago:Password01@dogdaycare.9jwdr.mongodb.net/"
+//   },
+//   Authentication: {
+//     GitHub: {
+//       ClientId: process.env.GITHUB_CLIENT_ID,
+//       ClientSecret: process.env.GITHUB_CLIENT_SECRET,
+//       CallbackURL: process.env.GITHUB_CALLBACK_URL || "http://localhost:3000/auth/github/callback"
+//     }
+//   }
+// };
+
+module.exports = configurations;
+
 require('dotenv').config();
 
-const configurations = {
+module.exports = {
   ConnectionStrings: {
-    MongoDB: process.env.MONGODB_URI || "mongodb+srv://alinerabago:Password01@dogdaycare.9jwdr.mongodb.net/"
+    MongoDB: process.env.MONGODB_URI || 'mongodb://localhost:27017/dogdaycare'
   },
   Authentication: {
     GitHub: {
       ClientId: process.env.GITHUB_CLIENT_ID,
       ClientSecret: process.env.GITHUB_CLIENT_SECRET,
-      CallbackURL: process.env.GITHUB_CALLBACK_URL || "http://localhost:3000/auth/github/callback"
+      CallbackURL: process.env.GITHUB_CALLBACK_URL || 'https://your-app-name.onrender.com/auth/github/callback'
+      // 'http://localhost:3000/auth/github/callback'
     }
   }
 };
-
-module.exports = configurations;
